4d9f094f10e3de14ff5d59ca45ab91c39a6981c6
manish <manish.sharma@tektronix.com>
2019-10-29 13:36:22 +0100

    reading font data.

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,185 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2019)
-and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
 #define SDL_MAIN_HANDLED
 
 #include <SDL.h>
 #include <stdio.h>
+#include <windows.h>
+#include <gl\gl.h>
+#include <gl\glu.h>
 
-//#undef main
+#define _UI_IMPLEMENTATION
+#include "ui.h"
+
+typedef struct
+{
+    SDL_Window*     window;
+    SDL_GLContext   context;
+    bool            render_quad;
+} global_state;
+
+global_state g_state = {
+    0,
+    {},
+    true
+};
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main()
+bool initGL()
 {
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
+    bool success = true;
+    GLenum error = GL_NO_ERROR;
+
+    //Initialize Projection Matrix
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
     
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+    //Check for error
+    error = glGetError();
+    if( error != GL_NO_ERROR )
+    {
+        //printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
+        success = false;
+    }
+
+    //Initialize Modelview Matrix
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity();
+
+    //Check for error
+    error = glGetError();
+    if( error != GL_NO_ERROR )
+    {
+        //printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
+        success = false;
+    }
+    //Initialize clear color
+    glClearColor( 0.f, 0.f, 0.f, 1.f );
+    
+    //Check for error
+    error = glGetError();
+    if( error != GL_NO_ERROR )
+    {
+        //printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
+        success = false;
+    }
+    
+    return success;
+}
+
+bool init()
+{
+    load_font_file("res/font.fnt");
+
+    //Initialization flag
+    bool success = true;
 
     //Initialize SDL
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+        success = false;
     }
     else
     {
+        //Use OpenGL 2.1
+        SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 2 );
+        SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 1 );
+
         //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
+        g_state.window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
+        if( g_state.window == NULL )
         {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+            success = false;
         }
         else
         {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
+            //Create context
+            g_state.context = SDL_GL_CreateContext( g_state.window );
+            if( g_state.context == NULL )
+            {
+                printf( "OpenGL context could not be created! SDL Error: %s\n", SDL_GetError() );
+                success = false;
+            }
+            else
+            {
+                //Use Vsync
+                if( SDL_GL_SetSwapInterval( 1 ) < 0 )
+                {
+                    printf( "Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError() );
+                }
 
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-
-            //Wait two seconds
-            SDL_Delay( 2000 );
+                //Initialize OpenGL
+                if( !initGL() )
+                {
+                    printf( "Unable to initialize OpenGL!\n" );
+                    success = false;
+                }
+            }
         }
     }
 
-    //Destroy window
-    SDL_DestroyWindow( window );
+    return success;
+}
 
-    //Quit SDL subsystems
-    SDL_Quit();
 
+void handleKeys( unsigned char key, int x, int y )
+{
+    //Toggle quad
+    if (key == 'q')
+    {
+        g_state.render_quad = !g_state.render_quad;
+    }
+}
+
+void update()
+{
+    //No per frame update needed
+}
+
+void render()
+{
+    //Clear color buffer
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    //Render quad
+    if (g_state.render_quad)
+    {
+        glBegin(GL_QUADS);
+            glVertex2f(-0.5f, -0.5f);
+            glVertex2f(0.5f, -0.5f);
+            glVertex2f(0.5f, 0.5f);
+            glVertex2f(-0.5f, 0.5f);
+        glEnd();
+    }
+}
+
+int main()
+{
+    init();
+    SDL_StartTextInput();
+    bool quit = false;
+    while (!quit)
+    {
+        SDL_Event e;
+        while (SDL_PollEvent(&e) != 0 )
+        {
+            if (e.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+            else if (e.type == SDL_TEXTINPUT)
+            {
+                int x = 0, y = 0;
+                SDL_GetMouseState(&x, &y);
+                handleKeys(e.text.text[0], x, y);
+            }
+        }
+        render();
+        SDL_GL_SwapWindow(g_state.window);
+    }
+    SDL_StopTextInput();
     return 0;
 }
