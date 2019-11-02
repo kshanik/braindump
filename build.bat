9e5c22d9098a1aeccf72fa8a4efcc0223343c888
manish <manish.sharma@tektronix.com>
2019-11-02 14:54:57 +0100

    created sdl static binaries.

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
 SET bat_file_path=%~dp0
 
-cl /Zi %bat_file_path%/src/main.cpp /I %bat_file_path%/3rdparty/sdl/include  /link %bat_file_path%/3rdparty/sdl/VisualC/x64/Debug/SDL2.lib OpenGL32.lib /SUBSYSTEM:CONSOLE
+rem debug
+cl /Zi %bat_file_path%/src/main.cpp /I %bat_file_path%/3rdparty/sdl/include  /link %bat_file_path%/3rdparty/bin/x64/Debug/SDL2.lib %bat_file_path%/3rdparty/bin/x64/Debug/SDL2main.lib OpenGL32.lib  gdi32.lib user32.lib shell32.lib  comdlg32.lib ole32.lib oleAut32.lib winmm.lib advapi32.lib version.lib Imm32.lib Setupapi.lib /SUBSYSTEM:CONSOLE
+
+rem release
+rem cl %bat_file_path%/src/main.cpp /I %bat_file_path%/3rdparty/sdl/include  /link %bat_file_path%/3rdparty/bin/x64/Release/SDL2.lib %bat_file_path%/3rdparty/bin/x64/Release/SDL2main.lib OpenGL32.lib  gdi32.lib user32.lib shell32.lib  comdlg32.lib ole32.lib oleAut32.lib winmm.lib advapi32.lib version.lib Imm32.lib Setupapi.lib /SUBSYSTEM:CONSOLE
